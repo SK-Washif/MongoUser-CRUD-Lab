2363a0fe44fb026ee45286f00690fdd743e5c39b
SK-Washif <washiffishal@gmail.com>
2025-10-26 21:54:20 +0600

    DELETE data from database and client,server site(properly)

--- simple-crud-client/src/components/Users.jsx
+++ simple-crud-client/src/components/Users.jsx
@@ -27,7 +27,7 @@ const Users = ({usersPromise}) => {
       console.log('data using after creating user in db:', data);
 
       if(data.insertedId){
-        newUser._id = data.insertedID;
+        newUser._id = data.insertedId;
         const newUsers = [...users, newUser];
         setUsers(newUsers);
         alert('User added Successfully!');
@@ -38,9 +38,28 @@ const Users = ({usersPromise}) => {
 
   }
 
+  const handleUserDelete = (id) =>{
+
+    console.log('delete this user', id);
+    fetch(`http://localhost:1000/users/${id}`,{
+      method:'DELETE'
+
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data.deletedCount){
+        const remainingUsers = users.filter(user => user._id !== id);
+        setUsers(remainingUsers);
+        console.log("after delete:",data);
+      }
+    })
+
+  }
+
   return (
     <div>
       <div>
+        <h3>Users: {users.length}</h3>
         <form onSubmit={handleAddUser}>
         <input name='name' type="text" />
         <br />
@@ -52,7 +71,10 @@ const Users = ({usersPromise}) => {
 
       <div>
         {
-          users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+          users.map(user => <p key={user._id}>
+            {user.name} : {user.email}
+            <button onClick={()=>handleUserDelete(user._id)}>X</button>
+            </p>)
         }
       </div>
     </div>
